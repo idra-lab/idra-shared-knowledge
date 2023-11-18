0043a1463029c70f9d13f446f417365c3928de06
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2023-11-18 10:14:48 +0100

    ROS2 Simulation Setup Notes on relevant nodes that must be present to carry out a simulation in CoppeliaSim.

---
+++ 01_ros_2/10_Simulation/00_index.rst
@@ -0,0 +1,11 @@
+================
+Simulation Setup
+================
+
+Setting up a simulation requires a lot of different nodes that interact with each other.
+The following is a graph showing nodes (and associated relationships) used in a simulation.
+
+.. graphviz:: simulation.dot
+   :align: center
+
+.. include:: 01_node_desc.rst
--- conf.py
+++ conf.py
@@ -9,6 +9,7 @@ author = 'Matteo Dalle Vedove'
 release = ''
 
 extensions = [
+              "sphinx.ext.graphviz",
               "sphinx_copybutton",
               "myst_parser"
               ]
@@ -18,3 +19,8 @@ source_suffix = ['.rst', '.md']
 pygments_style = 'sphinx'
 
 html_theme = 'furo'
+html_static_path = ['00_sphinx_data/_static']
+template_path = ['00_sphinx_data/_templates']
+
+def setup(app):
+    app.add_css_file('custom_container.css')
